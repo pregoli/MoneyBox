62afad1b48c4c981e01ad55ed1be9a44ee3bd62c
Paolo Regoli <Paolo.Regoli@coreview.com>
2020-09-04 23:40:40 +0200

    UT project added

---
+++ src/MoneyBox.App.Tests/Domain/AccountTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyBox.App.Tests.Domain
+{
+    public class AccountTests
+    {
+    }
+}
---
+++ src/MoneyBox.App.Tests/Features/WithdrawMoneyTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyBox.App.Tests.Features
+{
+    public class WithdrawMoneyTests
+    {
+    }
+}
